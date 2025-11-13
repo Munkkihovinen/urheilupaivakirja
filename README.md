2572d511426e31d26f107d90e0548816c7965d9d
munkkihovinen <sami.p.tonttila@gmail.com>
2025-11-13 18:56:01 +0200

    add register and login functionality with db

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # urheilupaivakirja
 
-Sovelluksessa käyttäjät pystyvät jakamaan urheilusuorituksiaan. Suorituksessa on laji, päivämäärä, ajallinen kesto, lajista riippuen mahdollisesti matka, sekä vapaa tekstikenttä, jossa voi antaa kuvauksen suorituksesta. Suoritukselle valittava laji ei ole vapaa tekstikenttä vaan valitaan sovelluksen tarjoamalta listalta.  
-Käyttäjä pystyy luomaan tunnuksen ja kirjautumaan sisään sovellukseen.  
-Käyttäjä pystyy lisäämään urheilusuorituksia ja muokkaamaan ja poistamaan niitä.  
-Käyttäjä näkee sovelluksessa omat ja muiden lisäämät suoritukset.  
-Käyttäjä pystyy etsimään suorituksia käyttäjänimen ja lajin perusteella ja järjestämään hakutulokset keston, matkan tai päivämäärän mukaan.  
-Käyttäjäsivu näyttää lajikohtaisesti suorituskerrat, yhteenlasketun ajan ja matkan, sekä listan kaikista suorituksista.  
-Käyttäjä pystyy lisäämään kommentteja omiin ja muiden suorituksiin. Suoritussivulla näytetään kaikki kommentit.  
+- Sovelluksessa käyttäjät pystyvät jakamaan urheilusuorituksiaan. Suorituksessa on laji, päivämäärä, ajallinen kesto, lajista riippuen mahdollisesti matka, sekä vapaa tekstikenttä, jossa voi antaa kuvauksen suorituksesta. Suoritukselle valittava laji ei ole vapaa tekstikenttä vaan valitaan sovelluksen tarjoamalta listalta.
+- Käyttäjä pystyy luomaan tunnuksen ja kirjautumaan sisään sovellukseen.
+- Käyttäjä pystyy lisäämään urheilusuorituksia ja muokkaamaan ja poistamaan niitä.
+- Käyttäjä näkee sovelluksessa omat ja muiden lisäämät suoritukset.
+- Käyttäjä pystyy etsimään suorituksia käyttäjänimen ja lajin perusteella ja järjestämään hakutulokset keston, matkan tai päivämäärän mukaan.
+- Käyttäjäsivu näyttää lajikohtaisesti suorituskerrat, yhteenlasketun ajan ja matkan, sekä listan kaikista suorituksista.
+- Käyttäjä pystyy lisäämään kommentteja omiin ja muiden suorituksiin. Suoritussivulla näytetään kaikki kommentit.
 
-Pääasiallinen tietokohde on urheilusuoritus ja toissijainen tietokohde on kommentti.
+- Pääasiallinen tietokohde on urheilusuoritus ja toissijainen tietokohde on kommentti.
---
+++ app.py
@@ -0,0 +1,70 @@
+import sqlite3
+from flask import Flask
+from flask import render_template, request, session, redirect
+from werkzeug.security import generate_password_hash, check_password_hash
+import db
+import config
+
+app = Flask(__name__)
+app.secret_key = config.secret_key
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+
+    # check if username is taken    
+    sql = "SELECT id FROM users WHERE username = ?"
+    taken = db.query(sql, [username])
+    if taken:
+        error = "VIRHE: tunnus on jo varattu"
+        return render_template("register.html", error=error)
+
+    # check if passwords match
+    if password1 != password2:
+        error = "VIRHE: salasanat eivät ole samat"
+        return render_template("register.html", error=error)
+    
+    # insert user to db
+    sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+    password_hash = generate_password_hash(password1)
+    db.execute(sql, [username, password_hash])
+    
+    return "Tunnus luotu"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        error = False
+        username = request.form["username"]
+        password = request.form["password"]
+        
+        sql = "SELECT password_hash FROM users WHERE username = ?"
+        db_row = db.query(sql, [username])
+
+        if not db_row:
+            error = True
+        else:
+            password_hash = db_row[0][0]
+            if check_password_hash(password_hash, password):
+                session["username"] = username
+                return redirect("/")
+            else:
+                error = True
+        return render_template('login.html', error=error)
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
